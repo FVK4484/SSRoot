6d0093a3f36a90010c75f9783ac6bc9cc01d5cd7
FVK4484 <180932485+FVK4484@users.noreply.github.com>
2026-09-24 18:20:01 +0200

    T01.
    Clase Teoría.

--- Teoria/T01/T01.Sol/T01.D04.CalculaMhz/CalculaMhz.cpp
+++ Teoria/T01/T01.Sol/T01.D04.CalculaMhz/CalculaMhz.cpp
@@ -1,23 +1,50 @@
 // Ejercicio final práctica 0
 // Funciona cambiando ESPERA_MS a 2000 ms?
-#define ESPERA_MS (500)
+#include <stdio.h>
+#include <Windows.h>
+#include <locale>
 
+int entero32;
+int32_t OtroEntero32;
+
+#define ESPERA_MS (500)
+#define _1MHzEnHerzios (1000000.0)
+
+int var = 5;
+int* p1 = &var;
+int p3, *p2;
+
+void modificaPar(int* ppar)
+{
+	//*ppar = 6;
+	(*ppar)++;
+}
 
 void ImprimeGhz() {
-	int antes, ahora;
+
+	modificaPar(&var);
+
+	char car;
+	long int antes, ahora = 0;
 	double VelocidadCPUGhz;
+
+	printf("Tamaño de int %d\n", sizeof(antes));
+	printf("Tamaño de char %d\n", sizeof(car));
+	printf("Tamaño de double %f\n", sizeof(VelocidadCPUGhz));
+	printf("Tamaño de int %d\n", sizeof(&antes));
+	printf("Tamaño de char %d\n", sizeof(&car));
+	printf("Tamaño de double %f\n", sizeof(&VelocidadCPUGhz));
 
 	antes = __rdtsc(); // Leo el valor del contador RDTSC y lo guardo
 	Sleep(ESPERA_MS); // Espero un tiempo determinado (en milisegundos)
 	ahora = __rdtsc();// Leo el valor del contador ahora
 
-	VelocidadCPUGhz = (ahora - antes) / (ESPERA_MS * _1MHzEnHerzios);
-	printf("\nLa frecuencia en Hz es: %d", VelocidadCPUGhz);
+	VelocidadCPUGhz = (double)(ahora - antes) / (ESPERA_MS * _1MHzEnHerzios);
+	printf("\nLa frecuencia en Hz es: %f", VelocidadCPUGhz);
 }
 
 
-#include <stdio.h>
-#include <locale>
+
 
 int main(int argc, char* argv[], char* envp[])
 {
